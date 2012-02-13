e5939d8b4231c5170622dd60d83533409b7421de
Dach-h <bunny.hop.md@gmail.com>
2012-02-13 19:10:16 +0900

    refactor

--- lib/lokka/popular_pages.rb
+++ lib/lokka/popular_pages.rb
@@ -35,29 +35,49 @@ module Lokka
         dimensions :page_title, :page_path
       end
 
-      def self.profile
-        unless @profile
+      class << self
+        attr_accessor :profile, :time_stamp
+
+        def response(option)
+          default =
+            { :limit      => 10,
+              :sort       => :pageviews.descending,
+              :start_date => (1.month.ago Time.now).beginning_of_month,
+              :end_date   => (1.month.ago Time.now).end_of_month }
+
+          profile.upvs(default.merge(option))
+        end
+
+        def profile
+          if updated?
+            if @profile
+              Thread.new {
+                @profile = _profile
+                @time_stamp = Time.now
+              }
+            else
+              @profile = _profile
+            end
+          end
+          @profile
+        end
+
+        def _profile
           Garb::Session.login(Option.popular_pages_email, Option.popular_pages_password)
-          @profile =
-            Garb::Management::Profile.all.detect {|p| p.web_property_id == Option.popular_pages_tracker}
+          Garb::Management::Profile.all.detect {|p| p.web_property_id == Option.popular_pages_tracker}
         end
-        @profile
-      end
 
-      def self.response(option)
-        default =
-          { :limit      => 10,
-            :sort       => :pageviews.descending,
-            :start_date => (1.month.ago Time.now).beginning_of_month,
-            :end_date   => (1.month.ago Time.now).end_of_month }
-
-        profile.upvs(default.merge(option))
-      end
-
-      def self.valid?
-        %w(popular_pages_tracker popular_pages_password popular_pages_email).all? do |attr|
-          Option.send(attr).present?
+        def updated?
+          return true unless @time_stamp
+          @time_stamp + (60 * 60 * 12) < Time.now
         end
+
+        def valid?
+          %w(popular_pages_tracker popular_pages_password popular_pages_email).all? do |attr|
+            Option.send(attr).present?
+          end
+        end
+
       end
     end
   end
