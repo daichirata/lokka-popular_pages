72a9b25fac2bd954709adc3b0269a4d5fa220706
Daic_h <bunny.hop.md@gmail.com>
2012-02-15 00:45:34 +0900

    refactor

--- lib/lokka/popular_pages.rb
+++ lib/lokka/popular_pages.rb
@@ -21,7 +21,7 @@ module Lokka
           return unless Analytics.valid?
 
           Analytics.response(option).map do |res|
-            Entry(res.page_path.sub(/\//, ""))
+            Entry(res.page_path.sub(/\//, "")) rescue nil
           end.compact
         end
       end
@@ -36,26 +36,25 @@ module Lokka
       end
 
       class << self
-        attr_accessor :profile, :time_stamp
 
-        def response(option)
-          default =
-            { :limit      => 10,
-              :sort       => :pageviews.descending,
-              :start_date => (1.month.ago Time.now).beginning_of_month,
-              :end_date   => (1.month.ago Time.now).end_of_month }
-
-          update_span = option.delete(:update_span) || 12
-          profile(update_span).upvs(default.merge(option))
+        def valid?
+          %w(popular_pages_tracker popular_pages_password popular_pages_email).all? do |attr|
+            Option.send(attr).present?
+          end
         end
 
-        def profile(span)
-          if update?(span)
-            if @profile
-              Thread.new { @profile = _profile }
+        def response(option)
+          @config =
+            option.delete(:config) || :last_month
+          profile.upvs(get_config.merge(option))
+        end
+
+        def profile
+          if update?
+            @profile = if @profile
+              Thread.new { _profile }
             else
-              @time_stamp = Time.now
-              @profile = _profile
+              _profile
             end
           end
           @profile
@@ -63,21 +62,45 @@ module Lokka
 
         def _profile
           Garb::Session.login(Option.popular_pages_email, Option.popular_pages_password)
-          Garb::Management::Profile.all.detect {|p| p.web_property_id == Option.popular_pages_tracker}
+          Garb::Management::Profile.all.detect do |prof|
+            prof.web_property_id == Option.popular_pages_tracker
+          end
         end
 
-        def update?(span)
-          return true unless @time_stamp
-
-          if @time_stamp + (60 * 60 * span.to_i) < Time.now
+        def update?
+          unless @time_stamp
             @time_stamp = Time.now
+            return true
+          end
+
+          case @config.to_s
+          when "last_month"
+            set_if { (1.month.from_now @time_stamp).beginning_of_month < Time.now }
+          when "last_week"
+            set_if { (1.week.from_now @time_stamp).beginning_of_week < Time.now }
           end
         end
 
-        def valid?
-          %w(popular_pages_tracker popular_pages_password popular_pages_email).all? do |attr|
-            Option.send(attr).present?
-          end
+        def set_if
+          @time_stamp = Time.now if yield
+        end
+
+        def get_config
+          send(@config) rescue last_month
+        end
+
+        def last_month
+          { :limit      => 10,
+            :sort       => :pageviews.descending,
+            :start_date => (1.month.ago Time.now).beginning_of_month,
+            :end_date   => (1.month.ago Time.now).end_of_month }
+        end
+
+        def last_week
+          { :limit      => 10,
+            :sort       => :pageviews.descending,
+            :start_date => (1.week.ago Time.now).beginning_of_week,
+            :end_date   => (1.week.ago Time.now).end_of_week }
         end
 
       end
